2a1c0192e84a58070910785bd6a9ae6c8afd1fa1
hyeok0008 <sun970830@gmail.com>
2026-08-31 02:00:37 +0900

    CI: 아직 의미 없는 자동생성 테스트는 건너뛰도록 수정

--- market-service/src/test/java/com/sajo/market_service/MarketServiceApplicationTests.java
+++ market-service/src/test/java/com/sajo/market_service/MarketServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.sajo.market_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class MarketServiceApplicationTests {
 
 	@Test
--- user-service/src/test/java/com/sajo/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/sajo/user_service/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.sajo.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class UserServiceApplicationTests {
 
 	@Test
