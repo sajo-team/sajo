70208d5b1aa0884e3f942025a2e53ea8d4045b4b
hyeok0008 <sun970830@gmail.com>
2026-09-01 12:47:19 +0900

    feat(User): User 엔티티 추가

---
+++ user-service/src/main/java/com/sajo/user_service/auth/domain/User.java
@@ -0,0 +1,50 @@
+package com.sajo.user_service.auth.domain;
+
+import com.sajo.common.entity.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseUpdatableEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Builder
+    private User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public static User of(String email, String password, String name) {
+        return User.builder()
+                .email(email)
+                .password(password)
+                .name(name)
+                .build();
+    }
+}
