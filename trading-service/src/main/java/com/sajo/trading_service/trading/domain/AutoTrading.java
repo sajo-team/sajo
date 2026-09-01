254da8a9c80b8f24e7255f47c7ff1483c4c9ab9b
TAEYOON EOM <djaxodbs0101@naver.com>
2026-09-01 15:56:47 +0900

    feat(trading): AutoTrading 엔티티 및 Repository 추가 #22
    - AutoTrading 엔티티
    - AutoTradingCommandRepository
    - AutoTrading 관련 에러코드 0004~0009

---
+++ trading-service/src/main/java/com/sajo/trading_service/trading/domain/AutoTrading.java
@@ -0,0 +1,54 @@
+package com.sajo.trading_service.trading.domain;
+
+import com.sajo.common.entity.BaseUpdatableEntity;
+import com.sajo.common.exception.BusinessException;
+import com.sajo.trading_service.trading.exception.TradingErrorCode;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "p_auto_tradings")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AutoTrading extends BaseUpdatableEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "strategy_id", nullable = false)
+    private UUID strategyId;
+
+    @Column(name = "enabled", nullable = false)
+    private Boolean enabled;
+
+    private AutoTrading(
+            UUID userId,
+            UUID strategyId
+    ){
+        this.userId = userId;
+        this.strategyId = strategyId;
+        enabled = true;
+    }
+
+    public static AutoTrading create(
+            UUID userId,
+            UUID strategyId
+    ) {
+        if (userId == null) {
+            throw new BusinessException(TradingErrorCode.INVALID_AUTO_TRADING, "사용자 ID는 필수입니다.");
+        }
+
+        if (strategyId == null) {
+            throw new BusinessException(TradingErrorCode.INVALID_AUTO_TRADING, "전략 ID는 필수입니다.");
+        }
+
+        return new AutoTrading(userId, strategyId);
+    }
+}
---
+++ trading-service/src/main/java/com/sajo/trading_service/trading/repository/command/AutoTradingCommandRepository.java
@@ -0,0 +1,13 @@
+package com.sajo.trading_service.trading.repository.command;
+
+import com.sajo.trading_service.trading.domain.AutoTrading;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface AutoTradingCommandRepository extends JpaRepository<AutoTrading, UUID> {
+    boolean existsByUserIdAndStrategyIdAndDeletedAtIsNull(UUID userId, UUID strategyId);
+
+    Optional<AutoTrading> findByIdAndUserIdAndDeletedAtIsNull(UUID autoTradingId, UUID userId);
+}
