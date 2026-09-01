3d263f409cbf77e1d06ffc4721126cc8150dead9
TAEYOON EOM <djaxodbs0101@naver.com>
2026-09-01 10:48:07 +0900

    feat(trading): 거래 한도 조회 서비스 구현 #5

--- trading-service/src/main/java/com/sajo/trading_service/trading/repository/command/TradingLimitCommandRepository.java
+++ trading-service/src/main/java/com/sajo/trading_service/trading/repository/command/TradingLimitCommandRepository.java
@@ -2,11 +2,9 @@ package com.sajo.trading_service.trading.repository.command;
 
 import com.sajo.trading_service.trading.domain.TradingLimit;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Repository
 public interface TradingLimitCommandRepository extends JpaRepository<TradingLimit, UUID> {
 
     boolean existsByUserId(UUID userId);
