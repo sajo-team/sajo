535c05fe2d32e8a232605937041057f00b600363
hyeok0008 <sun970830@gmail.com>
2026-09-01 12:48:06 +0900

    feat(UserRepository): 이메일 중복 확인 메서드 추가

---
+++ user-service/src/main/java/com/sajo/user_service/auth/domain/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sajo.user_service.auth.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByEmail(String email);
+}
