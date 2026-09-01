c5aa80b5a5a3948e2d7dd7b06f4ecb92e5673203
hyeok0008 <sun970830@gmail.com>
2026-09-01 16:02:04 +0900

    feat(UserCommandService): 회원가입 서비스 로직 구현

---
+++ user-service/src/main/java/com/sajo/user_service/auth/service/command/UserCommandService.java
@@ -0,0 +1,34 @@
+package com.sajo.user_service.auth.service.command;
+
+import com.sajo.common.exception.BusinessException;
+import com.sajo.user_service.auth.controller.dto.request.SignUpRequest;
+import com.sajo.user_service.auth.domain.User;
+import com.sajo.user_service.auth.exception.UserErrorCode;
+import com.sajo.user_service.auth.repository.command.UserCommandRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserCommandService {
+
+    private final UserCommandRepository userCommandRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User signUp(SignUpRequest request) {
+        if (userCommandRepository.existsByEmail(request.email())) {
+            throw new BusinessException(UserErrorCode.DUPLICATE_EMAIL);
+        }
+
+        User user = User.of(
+                request.email(),
+                passwordEncoder.encode(request.password()),
+                request.name()
+        );
+
+        return userCommandRepository.save(user);
+    }
+}
