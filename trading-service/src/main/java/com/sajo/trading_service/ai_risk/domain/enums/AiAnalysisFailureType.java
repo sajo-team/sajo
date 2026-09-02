8f1995883a676569ee0084b83e536a7c35d02041
leunbin <silverbin2105@gmail.com>
2026-09-02 11:55:44 +0900

    Feat(Ai) : AiRiskAnalysis 엔티티 추가

---
+++ trading-service/src/main/java/com/sajo/trading_service/ai_risk/domain/enums/AiAnalysisFailureType.java
@@ -0,0 +1,7 @@
+package com.sajo.trading_service.ai_risk.domain.enums;
+
+public enum AiAnalysisFailureType {
+    LLM_API_ERROR, //GPT API 호출 실패
+    RESPONSE_PARSE_ERROR, //GPT 응답 파싱 실패
+    VALIDATION_ERROR // GPT 응답 검증 실패
+}
---
+++ trading-service/src/main/java/com/sajo/trading_service/ai_risk/domain/enums/AiAnalysisStatus.java
@@ -0,0 +1,7 @@
+package com.sajo.trading_service.ai_risk.domain.enums;
+
+public enum AiAnalysisStatus {
+    PENDING,
+    COMPLETED,
+    FAILED
+}
