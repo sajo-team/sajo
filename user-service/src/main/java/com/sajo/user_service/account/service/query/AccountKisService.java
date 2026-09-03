a35fea70c619595eec94cf8253879a7eba4ed976
wootae <comijoa2@naver.com>
2026-09-03 12:04:57 +0900

    feat(account): access token 발급 cache 적용

--- user-service/src/main/java/com/sajo/user_service/account/service/query/AccountKisService.java
+++ user-service/src/main/java/com/sajo/user_service/account/service/query/AccountKisService.java
@@ -5,6 +5,7 @@ import com.sajo.user_service.account.client.dto.response.KISAccessTokenResponse;
 import com.sajo.user_service.account.controller.dto.response.AccessTokenResponse;
 import com.sajo.user_service.account.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -16,6 +17,11 @@ public class AccountKisService {
     private final AccountQueryService accountQueryService;
     private final KisClient kisClient;
 
+
+    // ToDo : 캐시 만료 시 kis 요청 방지 위해  분산락 적용
+    //  @Cacheable -> RedisTemplate 직접 사용으로 전환하면서,
+    //  KIS 응답의 expires_in 기준으로 TTL을 동적으로 계산하도록 같이 개선
+    @Cacheable(cacheNames = "kis-access-token", key = "#userId", sync = true)
     public AccessTokenResponse getKisAccessToken(UUID userId) {
         Account account = accountQueryService.getAccountByUserId(userId);
         KISAccessTokenResponse accessToken =
